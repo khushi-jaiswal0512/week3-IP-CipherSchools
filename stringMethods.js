045177b5ff1ae1394b21ff7dcc5b2cc74784958b
khushi jaiswal <119439224+khushi-jaiswal0512@users.noreply.github.com>
2023-01-10 22:12:33 +0530

    Add files via upload

---
+++ stringMethods.js
@@ -0,0 +1,27 @@
+let name = 'khushi'
+upper = name.toUpperCase()
+console.log(upper)
+
+// typeof funciton
+
+console.log(typeof name)
+
+// string slicing
+console.log(name.slice(0,4))
+
+// convert string to number
+name = +name
+// convert number to string
+name = name +""
+// string concatenation
+
+let firstName = 'khushi'
+let lastname = 'jaiswal'
+let fullName = firstName + lastname
+console.log(fullName)
+
+// template strings 
+// this is like formatted strings in python but the syntax is different
+let age = 17
+let aboutMe = `My name is ${name} and my age is ${age}`
+let aboutMe = `My name is ${name} and my age is ${age}`
